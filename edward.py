c7578b5297c6e58fbf496f1d9a5fc4ddfc05b125
powah999 <edwardwidjaja0507@gmail.com>
2023-08-26 18:31:26 +1000

    Update edward.py

--- edward.py
+++ edward.py
@@ -6,7 +6,7 @@ resp = requests.post("https://accounts.spotify.com/api/token", headers={"Content
 # Make a simple request
 BASE_URL = "https://api.spotify.com"
 headers = {
-    "Authorization": f"Bearer BQDTlaKNeRhN09uTN0iv4r7grdg966RkqnGZk16ghrlrMBvFURSgP87R8q-3abf9vR8-xDGSdUPi9ikHAFgdF4eFJirFGelj7a_25Z_jDi1Uce0YiqU"
+    "Authorization": "Bearer BQDTlaKNeRhN09uTN0iv4r7grdg966RkqnGZk16ghrlrMBvFURSgP87R8q-3abf9vR8-xDGSdUPi9ikHAFgdF4eFJirFGelj7a_25Z_jDi1Uce0YiqU"
 }
 
 track_id = "5ubvP9oKmxLUVq506fgLhk"
@@ -14,10 +14,12 @@ track_id = "5ubvP9oKmxLUVq506fgLhk"
 response = requests.get(BASE_URL + "/v1/audio-features/" + track_id, headers=headers)
 "BQDTlaKNeRhN09uTN0iv4r7grdg966RkqnGZk16ghrlrMBvFURSgP87R8q-3abf9vR8-xDGSdUPi9ikHAFgdF4eFJirFGelj7a_25Z_jDi1Uce0YiqU"
 
-response = requests.get(BASE_URL + "/v1/audio-features/", params={"ids": "id1,id2,id3"}, headers=headers)
-"BQDTlaKNeRhN09uTN0iv4r7grdg966RkqnGZk16ghrlrMBvFURSgP87R8q-3abf9vR8-xDGSdUPi9ikHAFgdF4eFJirFGelj7a_25Z_jDi1Uce0YiqU"
+print(response.json())
 
 print(response.json()['tempo'])
+
+response = requests.get(BASE_URL + "/v1/audio-features/", params={"ids": "id1,id2,id3"}, headers=headers)
+"BQDTlaKNeRhN09uTN0iv4r7grdg966RkqnGZk16ghrlrMBvFURSgP87R8q-3abf9vR8-xDGSdUPi9ikHAFgdF4eFJirFGelj7a_25Z_jDi1Uce0YiqU"
 
 
 # def main():
