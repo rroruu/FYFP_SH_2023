ae3fd3fccad4fcd9a9ff0ce0a4dddd9ab6d73ea2
powah999 <edwardwidjaja0507@gmail.com>
2023-08-26 20:23:56 +1000

    Update edward.py

--- edward.py
+++ edward.py
@@ -12,14 +12,14 @@ headers = {
 track_id = "5ubvP9oKmxLUVq506fgLhk"
 
 response = requests.get(BASE_URL + "/v1/audio-features/" + track_id, headers=headers)
-"BQDTlaKNeRhN09uTN0iv4r7grdg966RkqnGZk16ghrlrMBvFURSgP87R8q-3abf9vR8-xDGSdUPi9ikHAFgdF4eFJirFGelj7a_25Z_jDi1Uce0YiqU"
 
 print(response.json())
 
 print(response.json()['tempo'])
 
 response = requests.get(BASE_URL + "/v1/audio-features/", params={"ids": "id1,id2,id3"}, headers=headers)
-"BQDTlaKNeRhN09uTN0iv4r7grdg966RkqnGZk16ghrlrMBvFURSgP87R8q-3abf9vR8-xDGSdUPi9ikHAFgdF4eFJirFGelj7a_25Z_jDi1Uce0YiqU"
+
+print(response.json())
 
 
 # def main():
